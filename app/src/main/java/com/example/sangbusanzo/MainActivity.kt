b0e08f9bbf8646386520f7ffcde79a8ea3caf944
minjong kim <79445881+Kim-Min-Jong@users.noreply.github.com>
2023-08-17 10:27:01 +0900

    Revert "Login"

--- app/src/main/java/com/example/sangbusanzo/MainActivity.kt
+++ app/src/main/java/com/example/sangbusanzo/MainActivity.kt
@@ -3,11 +3,9 @@ package com.example.sangbusanzo
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class SignInPage : AppCompatActivity() {
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sign_in_page)
-        supportActionBar?.hide()
-
+        setContentView(R.layout.activity_main)
     }
 }
